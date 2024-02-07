73e723ba021840e9aed81ce1d8a82571528324c1
sudippahari1 <sudippahari1@gmail.com>
2024-02-07 18:52:08 +0530

    Done with the BG color chanaging Project

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    console.log(e);
+    if (e.target.id === 'grey') {
+      body.style.background = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.background = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.background = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.background = e.target.id;
+    }
+  });
+});
